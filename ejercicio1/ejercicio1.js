e54ab73de210f2144905e187826d2686ff685220
Isabela Cardona <1111481156@u.icesi.edu.co>
2025-04-04 20:22:10 -0500

    Se creo un prompt con toUpperCase

--- ejercicio1/ejercicio1.js
+++ ejercicio1/ejercicio1.js
@@ -3,3 +3,11 @@ const opciones = ["PIEDRA", "PAPEL", "TIJERA"];
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
+
+let computadora = opciones[getRandomInt(3)]
+
+let jugar = true
+
+while (jugar) {
+    let usuario = prompt("Juegue PIEDRA, PAPEL o TIJERA (Escriba SALIR para terminar").toUpperCase();
+}

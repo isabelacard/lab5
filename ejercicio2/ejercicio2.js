0cb3d95c80b180c0a20dca85b7d4bfe5faf6a26e
Isabela Cardona <1111481156@u.icesi.edu.co>
2025-04-04 21:22:38 -0500

    Se organizo el codigo - ejercicio2

--- ejercicio2/ejercicio2.js
+++ ejercicio2/ejercicio2.js
@@ -5,6 +5,7 @@ console.log("Arreglo original de frutas:", frutas);
 frutas.sort()
 console.log("Frutas ordenadas alfabéticamente:", frutas);
 
+
 function SingularAplural(palabra) {
     if (palabra.endsWith("s")) {
         return palabra
@@ -13,22 +14,23 @@ function SingularAplural(palabra) {
 }
 
 let frutasPlural = [];
-
 for (let fruta of frutas) {
     frutasPlural.push(SingularAplural(fruta));
 }
 console.log("Frutas en plural:", frutasPlural);
 
+
 function existeFruta(nombreFruta) {
     return frutas.includes(nombreFruta);
 }
-
 console.log("¿Existe la fruta mango'?: ", existeFruta("mango")); 
 console.log("¿Existe la fruta uva?: ", existeFruta("uva"));
 
 
+
 let frutasCortas = frutas.filter(fruta => fruta.length <= 4);
 console.log("Frutas con 4 letras o menos:", frutasCortas);
+
 
 function eliminarFruta(nombreFruta) {
     const index = frutas.indexOf(nombreFruta);

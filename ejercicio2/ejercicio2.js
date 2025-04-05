7a441c6d3e17b45463225d7b743477752e03230e
Isabela Cardona <1111481156@u.icesi.edu.co>
2025-04-04 20:53:30 -0500

    Se creo la funcion SingularAplural que recibe la palabra y la vuelve plural - ejercicio2

--- ejercicio2/ejercicio2.js
+++ ejercicio2/ejercicio2.js
@@ -3,3 +3,10 @@ let frutas = ["manzana", "banano", "uva", "pera", "sandia", "kiwi", "naranja"]
 console.log (frutas)
 
 frutas.sort()
+
+function SingularAplural(palabra) {
+    if (palabra.endsWith("s")) {
+        return palabra
+    }
+    return palabra + "s"
+}
